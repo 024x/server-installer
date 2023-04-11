819b6aa66e0d8a44c1efdf66a1717919bae3132e
024x <satyendrabongi@gmail.com>
2023-04-11 18:17:20 +0530

    ddgfdgd

--- install.sh
+++ install.sh
@@ -1,8 +1,3 @@
-apt-get update -y
-apt-gey upgrade -y
-apt-get install -y git
-apt-get install -y python3
-apt-get install -y python3-pip
 git clone https://github.com/S4tyendra/python-server
 cd "python-server"
 pip install pyrogram
