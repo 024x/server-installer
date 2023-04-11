76f79896b1ebf785f99a0dd4088d79595138c87b
024x <satyendrabongi@gmail.com>
2023-04-11 16:12:17 +0530

    initial commit

---
+++ install.sh
@@ -0,0 +1,9 @@
+apt-get update -y
+apt-gey upgrade -y
+apt-get install -y git
+apt-get install -y python3
+apt-get install -y python3-pip
+git clone https://github.com/S4tyendra/python-server
+cd "python-server"
+pip install -r requirements.txt
+chmod -x ./start.sh
